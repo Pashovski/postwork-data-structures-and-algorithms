efcecd5e4623070ea9820d94892605c46ca8111c
Pasha <pashdoctor@gmail.com>
2022-01-14 08:19:18 -0500

    got it to work my way :)

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -1,5 +1,4 @@
 function romanNumeral(string) {
-  // type your code here
   // build object that give us integer values for each single symbol numeral
   const romanNumObj = {
       'I': 1,
@@ -10,29 +9,19 @@ function romanNumeral(string) {
       'D': 500,
       'M': 1000
   };
-  // if string length is greater than 1
+
   if (string.length > 1) {
-      // then turn string into array
-      const numeralArr = string.split('');
-      // start our final integer value at 0
-      let integer = 0;
-      // loop over array and compare its values using our romanNumObj
-      // RULES:
-      // if CURR is > PREV then CURR - PREV
-      // else CURR < PREV then CURR + PREV
-      numeralArr.forEach((el, i, arr) => {
-          const prev = romanNumObj[arr[i - 1]];
-          const curr = romanNumObj[el];
-          let added = 0;
-          if (curr > prev) {
-              added = curr - prev;
-              integer += added - prev;
+      let integer = romanNumObj[string[string.length - 1]];
+      for (let i = string.length - 1; i > 0; i--) {
+          const curr = romanNumObj[string[i]];
+          const prev = romanNumObj[string[i - 1]];
+          if (prev < curr) {
+              integer = integer - prev;
           } else {
-              integer += curr;
+              integer = integer + prev;
           }
-      })
+      }
       return integer;
-  // otherwise it's a single symbol numeral and its value can be derived directly from our romanNumObj object
   } else {
       return romanNumObj[string];
   }
